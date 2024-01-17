919e2ebec9d0d78bc2d9fcb7d5c6eb362fb361a7
jarathomas <jaratthomas@gmail.com>
2024-01-17 16:57:40 -0500

    added c++ sampler module

--- setup.py
+++ setup.py
@@ -1,50 +1,14 @@
-import os
-from codecs import open
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-about = {}
-with open(os.path.join(here, "insilicova", "__version__.py"), "r", "utf-8") as f:
-    exec(f.read(), about)
-
-with open("README.md", "r", "utf-8") as f:
-    readme = f.read()
-
+ext_modules = [
+    Pybind11Extension(
+        "cpp",
+        ["insilicova/_sampler/sampler.cpp"],
+    ),
+]
 
 setup(
-    name=about["__title__"],
-    version=about["__version__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    description=about["__description__"],
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url=about["__url__"],
-    license=about["__license__"],
-    packages=find_namespace_packages(),
-    include_package_data=True,
-    package_dir={"": "."},
-    package_data={
-        "insilicova.data": ["*.csv"],
-    },
-    install_requires=[
-        "pandas",
-        "xlrd",
-        "numpy",
-        "vacheck",
-        "interva",
-        "statsmodels",
-        "scipy",
-        "tqdm",
-        ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
 )

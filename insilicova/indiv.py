e2692d74e09bb5dd082dfbbd03cabd0455591c7a
jarathomas <jaratthomas@gmail.com>
2024-03-04 17:04:10 -0500

    moved sampler functions for individuals over to indiv.py; updated tox info to use installed version of this package

--- insilicova/indiv.py
+++ insilicova/indiv.py
@@ -7,24 +7,22 @@ insilicova.indiv
 This module contains functions for getting and updating individual COD
 probabilities from InSilicoVA model fits.
 """
-import numpy as np
-
 from insilicova.structures import InSilico
 from insilicova.exceptions import ArgumentException, DataException
-from insilicova.sampler import Sampler
+# from insilicova.sampler import Sampler
 from insilicova.utils import get_vadata
 import warnings
-from pandas import DataFrame, concat
-from numpy import apply_along_axis, array, delete, nan, ones, zeros
+import pandas as pd
+import numpy as np
 from typing import Dict, Union
 
 
 def get_indiv(insilico_obj: InSilico,
-              data: Union[None, DataFrame],
+              data: Union[None, pd.DataFrame],
               ci: float = 0.95,
               is_aggregate: bool = False,
               by: Union[None, list] = None,
-              is_sample: bool = False) -> DataFrame:
+              is_sample: bool = False) -> pd.DataFrame:
     """
     This function calculates individual probabilities for each death and
     provide posterior credible intervals for each estimate. The default set up
@@ -50,14 +48,14 @@ def get_indiv(insilico_obj: InSilico,
     :returns: Individual mean COD distribution array (mean); individual median
     COD distribution array (median); individual lower & upper bounds for each
     COD probability (lower & upper).
-    :rtype: DataFrame
+    :rtype: pandas DataFrame
     """
     id = insilico_obj.id
     # if grouping is provided
     if is_aggregate:
         if data is None:
             raise ArgumentException("Data not provided for grouping")
-        va_data = DataFrame(data).copy()
+        va_data = pd.DataFrame(data).copy()
         # check by variable
         if by is None:
             warnings.warn("No groups specified, aggregate for all deaths")
@@ -99,40 +97,40 @@ def get_indiv(insilico_obj: InSilico,
         datagroup = datagroup_all["final_group"][index_tmp].copy()
         datagroup = datagroup.dropna()
         allgroups = list(datagroup.sort_values().unique())
-        datagroup = [allgroups.index(i) if i in allgroups else nan
+        datagroup = [allgroups.index(i) if i in allgroups else np.nan
                      for i in datagroup]
-        datagroup_all["final_group"] = [allgroups.index(i) if i in allgroups else nan
+        datagroup_all["final_group"] = [allgroups.index(i) if i in allgroups else np.nan
                      for i in datagroup_all["final_group"]]
         n_groups = len(allgroups)
 
     # 3d array of csmf to be Nsub * Nitr * C
     if insilico_obj.subpop is None:
-        csmf = zeros((1, ) + insilico_obj.csmf.shape)
+        csmf = np.zeros((1, ) + insilico_obj.csmf.shape)
         csmf[0] = insilico_obj.csmf.copy()
-        subpop = zeros(len(insilico_obj.id), dtype=int)
+        subpop = np.zeros(len(insilico_obj.id), dtype=int)
     else:
         n_sub = len(insilico_obj.csmf)
         dims = (n_sub, ) + insilico_obj.csmf[0].shape
-        csmf = zeros(dims)
+        csmf = np.zeros(dims)
         label_sub = list(insilico_obj.subpop.unique())
         label_sub.sort()
         for i in range(n_sub):
             csmf[i, :, :] = insilico_obj.csmf[i]
         subpop = [label_sub.index(i) for i in insilico_obj.subpop]
-        subpop = array(subpop)
+        subpop = np.array(subpop)
     if insilico_obj.external:
-        csmf = delete(csmf, insilico_obj.external_causes, axis=2)
-        csmf = apply_along_axis(lambda x: x/sum(x) if sum(x) != 0 else x,
-                                2,
-                                csmf)
+        csmf = np.delete(csmf, insilico_obj.external_causes, axis=2)
+        csmf = np.apply_along_axis(lambda x: x/sum(x) if sum(x) != 0 else x,
+                                   2,
+                                   csmf)
     # get condprob to be Nitr * S * C array
     if insilico_obj.update_cond_prob is False:
-        condprob = zeros((csmf.shape[1], ) + insilico_obj.probbase.shape)
+        condprob = np.zeros((csmf.shape[1], ) + insilico_obj.probbase.shape)
         for i in range(csmf.shape[1]):
             condprob[i, :, :] = insilico_obj.probbase.copy()
     else:
         if insilico_obj.keep_probbase_level:
-            condprob = zeros(
+            condprob = np.zeros(
                 (insilico_obj.conditional_probs.shape[0], ) +
                 insilico_obj.probbase.shape)
             for i in range(condprob.shape[0]):
@@ -143,8 +141,8 @@ def get_indiv(insilico_obj: InSilico,
                 tmp_idx = [list(insilico_obj.conditional_probs).index(j) for
                            j in insilico_obj.probbase.flatten("F")]
                 temp = insilico_obj.conditional_probs.iloc[i, tmp_idx]
-                temp = array(temp).reshape(insilico_obj.probbase.shape,
-                                           order="F")
+                temp = np.array(temp).reshape(insilico_obj.probbase.shape,
+                                              order="F")
                 condprob[i, :, :] = temp
         else:
             condprob = insilico_obj.conditional_probs.copy()
@@ -154,11 +152,11 @@ def get_indiv(insilico_obj: InSilico,
     # csmf = csmf.flatten("F")
     if is_sample:
         print("Calculating individual COD posterior draws\n")
-        indivprob = Sampler.IndivProbSample(
+        indivprob = IndivProbSample(
             data=insilico_obj.data_final.to_numpy(),
             impossible=insilico_obj.impossible_causes,
             csmf=csmf,
-            subpop=subpop,
+            subpop=subpop.tolist(),
             condprob=condprob,
             Nsub=n_sub,
             Nitr=n_itr,
@@ -213,15 +211,15 @@ def get_indiv(insilico_obj: InSilico,
         # TODO: test if list(insilico_obj.indiv_prob) should be
         #       indivprob_cause_names (not using it)
         colnames = ["ID", "Iteration"] + list(insilico_obj.indiv_prob)
-        indivprob_ext = DataFrame(indivprob_ext, columns=colnames)
+        indivprob_ext = pd.DataFrame(indivprob_ext, columns=colnames)
         return indivprob_ext
     elif (not is_aggregate) and (not is_sample):
         print("Calculating individual COD distributions...\n")
-        indiv = Sampler.IndivProb(
+        indiv = IndivProb(
             data=insilico_obj.data_final.to_numpy(),
             impossible=insilico_obj.impossible_causes,
             csmf=csmf,
-            subpop=subpop,
+            subpop=subpop.tolist(),
             condprob=condprob,
             p0=(1 - ci)/2,
             p1=1 - (1 - ci)/2,
@@ -272,27 +270,27 @@ def get_indiv(insilico_obj: InSilico,
             median = np.concatenate((median, ext_probs), axis=0)
             lower = np.concatenate((lower, ext_probs), axis=0)
             upper = np.concatenate((upper, ext_probs), axis=0)
-        mean = DataFrame(mean, columns=list(insilico_obj.indiv_prob))
+        mean = pd.DataFrame(mean, columns=list(insilico_obj.indiv_prob))
         mean.insert(loc=0, column="Statistic", value="mean")
         mean.insert(loc=0, column="ID", value=id_out)
-        median = DataFrame(median, columns=list(insilico_obj.indiv_prob))
+        median = pd.DataFrame(median, columns=list(insilico_obj.indiv_prob))
         median.insert(loc=0, column="Statistic", value="median")
         median.insert(loc=0, column="ID", value=id_out)
-        lower = DataFrame(lower, columns=list(insilico_obj.indiv_prob))
+        lower = pd.DataFrame(lower, columns=list(insilico_obj.indiv_prob))
         lower.insert(loc=0, column="Statistic", value="lower")
         lower.insert(loc=0, column="ID", value=id_out)
-        upper = DataFrame(upper, columns=list(insilico_obj.indiv_prob))
+        upper = pd.DataFrame(upper, columns=list(insilico_obj.indiv_prob))
         upper.insert(loc=0, column="Statistic", value="upper")
         upper.insert(loc=0, column="ID", value=id_out)
-        indiv_out = concat((mean, median, lower, upper), axis=0)
+        indiv_out = pd.concat((mean, median, lower, upper), axis=0)
         return indiv_out
     else:
         print("Aggregating individual COD distributions...\n")
-        indiv = Sampler.AggIndivProb(
+        indiv = AggIndivProb(
             data=insilico_obj.data_final.to_numpy(),
             impossible=insilico_obj.impossible_causes,
             csmf=csmf,
-            subpop=subpop,
+            subpop=subpop.tolist(),
             condprob=condprob,
             group=datagroup,
             Ngroup=n_groups,
@@ -348,7 +346,7 @@ def get_indiv(insilico_obj: InSilico,
         else:
             id_out = id
             ext_probs = None
-        indiv = DataFrame(indiv, columns=list(insilico_obj.indiv_prob))
+        indiv = pd.DataFrame(indiv, columns=list(insilico_obj.indiv_prob))
         if by is None:
             indiv = indiv.transpose()
             indiv.columns = ["Mean", "Median", "Lower", "Upper"]
@@ -362,7 +360,7 @@ def get_indiv(insilico_obj: InSilico,
         return indiv
 
 def update_indiv(insilico_obj: InSilico,
-                 data: Union[None, DataFrame],
+                 data: Union[None, pd.DataFrame],
                  ci: float = 0.95,
                  is_aggregate: bool = False,
                  by: Union[None, list] = None,
@@ -377,7 +375,7 @@ def update_indiv(insilico_obj: InSilico,
     :type insilico_obj: InSilico
     :param data: Data for the fitted InSilico object. The first column of the
     data should be the ID that matches the InSilico fitted model.
-    :type data: DataFrame
+    :type data: pandas DataFrame
     :param ci:  Width of credible interval for posterior estimates.
     :type ci: float
     :param is_aggregate: Indicator for constructing aggregated distribution
@@ -408,3 +406,141 @@ def update_indiv(insilico_obj: InSilico,
     # TODO: need to account for different options (e.g., is_sample=True,
     #       is_aggregate=True and by=None)
     return insilico_obj
+
+def IndivProb(data: np.ndarray,
+              impossible: np.ndarray,
+              csmf: np.ndarray,
+              subpop: list,
+              condprob: np.ndarray,
+              p0: float,
+              p1: float,
+              Nsub: int,
+              Nitr: int,
+              C: int,
+              S: int) -> np.ndarray:
+
+    N = data.shape[0]
+
+    zero_matrix = np.ones((N, C))
+    if impossible.shape[1] == 2:
+        for i in range(N):
+            for k in range(impossible.shape[0]):
+                if data[i, impossible[k, 1]] == 1:
+                    zero_matrix[i, impossible[k, 0]] = 0
+    allp = np.zeros((N, C, Nitr))
+    for i in range(N):
+        sub = subpop[i]
+        for t in range(Nitr):
+            allp[i, :, t] = csmf[sub, t, :] * zero_matrix[i, :]
+            factor = np.ones((S, C))
+            index_eq1 = data[i, :] == 1
+            index_eq0 = data[i, :] == 0
+            factor[index_eq1, :] = condprob[t, index_eq1, :].copy()
+            factor[index_eq0, :] = 1 - condprob[t, index_eq0, :].copy()
+            index_eq1_or_eq0 = np.isin(data[i, :], [0, 1])
+            allp[i, :, t] *= np.product(factor[index_eq1_or_eq0], axis=0)
+            sum_allp = sum(allp[i, :, t])
+            if sum_allp > 0:
+                allp[i, :, t] = allp[i, :, t] / sum_allp
+    out = np.zeros((N * 4, C))
+    out[:N, :] = allp.mean(axis=2)
+    out[N:(2*N), :] = np.percentile(a=allp, q=0.5, axis=2)
+    out[(2 * N):(3 * N), :] = np.percentile(a=allp, q=p0, axis=2)
+    out[(3 * N):(5 * N), :] = np.percentile(a=allp, q=p1, axis=2)
+    return out
+
+def AggIndivProb(data: np.ndarray,
+                 impossible: np.ndarray,
+                 csmf: np.ndarray,
+                 subpop: list,
+                 condprob: np.ndarray,
+                 group: list,
+                 Ngroup: int,
+                 p0: float,
+                 p1: float,
+                 Nsub: int,
+                 Nitr: int,
+                 C: int,
+                 S: int) -> np.ndarray:
+
+    N = data.shape[0]
+    zero_matrix = np.ones((N, C))
+    if impossible.shape[1] == 2:
+        for i in range(N):
+            for k in range(impossible.shape[0]):
+                if data[i, impossible[k, 1]] == 1:
+                    zero_matrix[i, impossible[k, 0]] = 0
+    allp = np.zeros((Ngroup, C, Nitr))
+    size = np.zeros(Ngroup, dtype=int)
+    for i in range(N):
+        # get subpop index for death i
+        sub = subpop[i]
+        g = group[i]
+        size[g] += 1
+        for t in range(Nitr):
+            tmp = csmf[sub, t, :] * zero_matrix[i, :]
+            factor = np.ones((S, C))
+            index_eq1 = data[i, :] == 1
+            index_eq0 = data[i, :] == 0
+            factor[index_eq1, :] = condprob[t, index_eq1, :].copy()
+            factor[index_eq0, :] = 1 - condprob[t, index_eq0, :].copy()
+            index_eq1_or_eq0 = np.isin(data[i, :], [0, 1])
+            tmp *= np.product(factor[index_eq1_or_eq0], axis=0)
+            sum_tmp = sum(tmp)
+            if sum_tmp == 0:
+                allp[g, :, t] = tmp
+            else:
+                allp[g, :, t] = tmp / sum_tmp
+
+    out = np.zeros((Ngroup * 4, C + 1))
+    for i in range(Ngroup):
+        for c in range(C):
+            out[i, c] = allp[i, c, :].mean() / size[i]
+            out[i + Ngroup, c] = np.percentile(allp[i, c, :], 0.5) / size[
+                i]
+            out[i + Ngroup * 2, c] = np.percentile(allp[i, c, :], p0) / \
+                                     size[i]
+            out[i + Ngroup * 3, c] = np.percentile(allp[i, c, :], p1) / \
+                                     size[i]
+        out[i][C] = size[i]
+        out[i + Ngroup, C] = size[i]
+        out[i + Ngroup * 2, C] = size[i]
+        out[i + Ngroup * 3, C] = size[i]
+
+    return out
+
+def IndivProbSample(data: np.ndarray,
+                    impossible: np.ndarray,
+                    csmf: np.ndarray,
+                    subpop: list,
+                    condprob: np.ndarray,
+                    Nsub: int,
+                    Nitr: int,
+                    C: int,
+                    S: int) -> np.ndarray:
+
+    N = data.shape[0]
+    zero_matrix = np.ones((N, C))
+    if impossible.shape[1] == 2:
+        for i in range(N):
+            for k in range(impossible.shape[0]):
+                # if data[i, impossible[k, 1] - 1] == 1:
+                #     zero_matrix[i, impossible[k, 0] - 1] = 0
+                if data[i, impossible[k, 1]] == 1:
+                    zero_matrix[i, impossible[k, 0]] = 0
+    allp = np.zeros((N, C, Nitr))
+    for i in range(N):
+        sub = subpop[i]
+        for t in range(Nitr):
+            allp[i, :, t] = csmf[sub, t, :] * zero_matrix[i, :]
+            factor = np.ones((S, C))
+            index_eq1 = data[i, :] == 1
+            index_eq0 = data[i, :] == 0
+            factor[index_eq1, :] = condprob[t, index_eq1, :].copy()
+            factor[index_eq0, :] = 1 - condprob[t, index_eq0, :].copy()
+            index_eq1_or_eq0 = np.isin(data[i, :], [0, 1])
+            allp[i, :, t] *= np.product(factor[index_eq1_or_eq0], axis=0)
+            sum_allp = sum(allp[i, :, t])
+            if sum_allp > 0:
+                allp[i, :, t] = allp[i, :, t]/sum_allp
+    return allp

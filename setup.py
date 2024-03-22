f3eda98a633bcc5b118d12d0d62471d39cb443a7
jarathomas <jaratthomas@gmail.com>
2024-03-22 17:29:43 -0400

    fixed error adding to Include path for windows

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@ import platform
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-WIN_BOOST_PATH = ""
 if platform.system() == "Windows":
-    WIN_BOOST_PATH = "C:\\boost_1_82_0"
-
-ext_modules = [
-    Pybind11Extension(
-        "insilicova._sampler._sampler",
-        ["src/insilicova/_sampler/sampler.cpp"],
-        # extra_compile_args=["-g"]
-        include_dirs=[WIN_BOOST_PATH],
-    ),
-]
+    ext_modules = [
+        Pybind11Extension(
+            "insilicova._sampler._sampler",
+            ["src/insilicova/_sampler/sampler.cpp"],
+            include_dirs=["C:\\boost_1_82_0"],
+        ),
+    ]
+else:
+    ext_modules = [
+        Pybind11Extension(
+            "insilicova._sampler._sampler",
+            ["src/insilicova/_sampler/sampler.cpp"],
+            # extra_compile_args=["-g"]
+        ),
+    ]
 
 setup(
     ext_modules=ext_modules,

a8c6b2374e2ac97343093d58626a7a25f9d45f0e
Jason Thomas <jarathomas@gmail.com>
2024-03-22 16:02:47 -0400

    setting up windows workflow with boost path in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import platform
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+WIN_BOOST_PATH = ""
+if platform.system() == "Windows":
+    WIN_BOOST_PATH = "C:\\boost_1_82_0"
 
 ext_modules = [
     Pybind11Extension(
         "insilicova._sampler._sampler",
         ["src/insilicova/_sampler/sampler.cpp"],
         # extra_compile_args=["-g"]
+        include_dirs=[WIN_BOOST_PATH],
     ),
 ]
 

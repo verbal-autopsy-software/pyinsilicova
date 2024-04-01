9efb577683e5eb917789cdd49408dc07e0f99307
jarathomas <jaratthomas@gmail.com>
2024-04-01 16:31:46 -0400

    only setting version in pyproject.toml

--- src/insilicova/__init__.py
+++
@@ -1,9 +0,0 @@
-from .__version__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-)
--- src/insilicova/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = "insilicova"
-__description__ = "Python implementation of the InSilicoVA Algorithm."
-__url__ = "https://github.com/verbal-autopsy-software/pyinsilicova"
-__version__ = "0.0.2"
-__author__ = "Jason Thomas & Sherry Zhao"
-__author_email__ = "jarathomas@gmail.com"
-__license__ = "GPLv3"
